95098a51c872a95c3c404b6d1168f0ef98746a00
NATHAN GEHRKE <rl107106@rlas-macair-22.rlas-116.org>
2018-09-25 11:39:39 -0500

    Initial Commit

---
+++ Display Number/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  Display Number
+//
+//  Created by NATHAN GEHRKE on 9/24/18.
+//  Copyright © 2018 Nathan Gehrke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var numberLabel: UILabel!
+    @IBOutlet weak var numberTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    
+        
+        
+        
+    
+    }
+
+    @IBAction func numberOfButtonTaped(_ sender: Any) {
+        let numberToDisplay = numberTextField.text 
+        
+        
+        
+        
+        
+    
+    }
+    
+
+
+}
